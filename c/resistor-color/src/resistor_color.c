99b418299f23bbc8501b4b63a6c2ddd5b7485250
Hernan <hernan.rod.98@gmail.com>
2021-03-27 20:02:43 -0300

    c: resistor colors

---
+++ c/resistor-color/src/resistor_color.c
@@ -0,0 +1,14 @@
+#include "resistor_color.h"
+
+static const resistor_band_t all_colors[] = {
+    BLACK, BROWN, RED, ORANGE, YELLOW,
+    GREEN, BLUE, VIOLET, GREY, WHITE
+};  
+
+uint16_t color_code(resistor_band_t color){
+    return (uint16_t)color;
+}
+
+const resistor_band_t* colors(void) {
+    return all_colors;
+}
