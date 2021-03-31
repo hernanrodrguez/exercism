7551159bd6540ac6a3c2245cbcd8e8dc0fbd4f82
Hernan <hernan.rod.98@gmail.com>
2021-03-31 12:10:50 -0300

    javascript: resistor color

---
+++ javascript/resistor-color/resistor-color.js
@@ -0,0 +1,14 @@
+//
+// This is only a SKELETON file for the 'Resistor Color' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export const colorCode = (color) => {
+  for(var i=0; i<COLORS.length; i++){
+    if(color == COLORS[i])
+      return i;
+  }
+  return -1;
+};
+
+export const COLORS = ['black','brown','red','orange','yellow','green','blue','violet','grey','white'];
